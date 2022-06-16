21d023d170d0aac71ff2026539a7cc3be81337c5
Cagatay Sarioglan <sarioglancgty@outlook.com>
2022-06-16 11:04:49 +0300

    new files added

--- GY_JAVA_101/Switch Break/src/Driver.java
+++ GY_JAVA_101/Switch Break/src/Driver.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Driver {
+import java.util.Scanner;
+
+public class Driver {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Haftanın günü : ");
+        int gun = scanner.nextInt();
+
+        switch (gun) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Hafta içi");
+                break;
+            case 6:
+            case 7:
+                System.out.println("Haftasonu");
+                break;
+        }
+    }
 }
--- GY_JAVA_101/SwitchKosuluLab/src/Driver.java
+++ GY_JAVA_101/SwitchKosuluLab/src/Driver.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Driver {
+import java.util.Scanner;
+
+public class Driver {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("1. Sayı: ");
+        float x = Float.parseFloat(scanner.nextLine());
+        System.out.print("2. Sayı: ");
+        float y = Float.parseFloat(scanner.nextLine());
+        System.out.print("İşlem: ");
+        char islem = scanner.nextLine().charAt(0);
+
+        switch (islem) {
+            case '+':
+                System.out.println("Sonuç : " + (x+y));
+                break;
+            case '-':
+                System.out.println("Sonuç : " + (x-y));
+                break;
+            case '*':
+                System.out.println("Sonuç : " + (x*y)); // degub modda
+                break;
+            case '/':
+                System.out.println("Sonuç : " + (x/y));
+                break;
+            default:
+                System.out.println("Sadece dört işlem yapabilirsiniz!");
+                break;
+        }
+
+
+    }
 }
