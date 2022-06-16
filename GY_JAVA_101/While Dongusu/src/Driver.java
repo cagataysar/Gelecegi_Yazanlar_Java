0626ecaf0ae0eac9d4633ea8a984284921af5ce9
Cagatay Sarioglan <sarioglancgty@outlook.com>
2022-06-16 17:55:14 +0300

    new files added

---
+++ GY_JAVA_101/While Dongusu/src/Driver.java
@@ -0,0 +1,26 @@
+import java.util.Date;
+import java.util.Scanner;
+
+public class Driver {
+    public static void main(String[] args) throws InterruptedException {
+
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+
+        /*while (true) {
+            System.out.println(new Date());
+            Thread.sleep(1000); //Her 1 saniyede o anki saati ekrana bastırdık
+        }*/
+
+        /*Scanner scanner = new Scanner(System.in);
+        while (true) {
+            //String text = scanner.nextLine();
+            //System.out.println(text.toUpperCase());
+            int i = scanner.nextInt();
+            System.out.println(Math.pow(i,2));
+        }*/
+    }
+}
