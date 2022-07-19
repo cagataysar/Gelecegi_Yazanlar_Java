5c7ee449d62d4da8cc55c484519fdf6e1772df9f
Çağatay Sarıoğlan <sarioglancgty@outlook.com>
2022-07-19 10:30:07 +0300

    Project created.

--- GY_JAVA_301_3/Metot Referans/src/Driver.java
+++ GY_JAVA_301_3/Metot Referans/src/Driver.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Driver {
+import java.util.ArrayList;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<>();
+        integers.add(2);
+        integers.add(5);
+        integers.add(0);
+        integers.add(6);
+
+        integers.forEach((s) -> System.out.println(s));
+    }
 }
